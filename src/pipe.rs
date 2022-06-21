6de2a4ec0e7bcfaddc5599a5b33bfbf6d0d41f36
Kile Asmussen <kile.asmussen@gmail.com>
2022-06-21 11:31:38 +0200

    formatting

--- src/pipe.rs
+++ src/pipe.rs
@@ -1,29 +1,40 @@
-
-
-trait Monadics : Sized {
-
-    fn tap<F>(&self, f: F) -> &Self where F: FnOnce(&Self) {
+trait Monadics: Sized {
+    fn tap<F>(&self, f: F) -> &Self
+    where
+        F: FnOnce(&Self),
+    {
         f(self);
         self
     }
 
-    fn with<F>(&mut self, f: F) -> &mut Self where F: FnOnce(&mut Self) {
+    fn with<F>(&mut self, f: F) -> &mut Self
+    where
+        F: FnOnce(&mut Self),
+    {
         f(self);
         self
     }
 
-    fn pipe<F, R>(self, f: F) -> R where F: FnOnce(Self) -> R {
+    fn pipe<F, R>(self, f: F) -> R
+    where
+        F: FnOnce(Self) -> R,
+    {
         f(self)
     }
 
-    fn view<F, R>(&self, f: F) -> R where F: FnOnce(&Self) -> R {
+    fn view<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&Self) -> R,
+    {
         f(self)
     }
 
-    fn then<F, R>(&mut self, f: F) -> R where F: FnOnce(&mut Self) -> R {
+    fn then<F, R>(&mut self, f: F) -> R
+    where
+        F: FnOnce(&mut Self) -> R,
+    {
         f(self)
     }
-
 }
 
 impl<T> Monadics for T {}
